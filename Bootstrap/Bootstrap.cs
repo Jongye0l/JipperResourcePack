27124bd691c0434bfbf07c772a9301f9b28787f1
Jongyeol <bcpjy1233@gmail.com>
2024-10-01 21:14:39 +0900

    fix: Bootstrap namespace Identification

--- Bootstrap/Bootstrap.cs
+++ Bootstrap/Bootstrap.cs
@@ -1,7 +1,7 @@
 ﻿using JALib.Bootstrap;
 using UnityModManagerNet;
 
-namespace JipperResourcePack {
+namespace Bootstrap {
     public class Bootstrap {
         public static void Setup(UnityModManager.ModEntry modEntry) => JABootstrap.Load(modEntry);
     }
