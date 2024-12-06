3f912fe9518202c18359c5bd418ad13c4499d4c2
Jongyeol <bcpjy1233@gmail.com>
2024-12-06 22:08:05 +0900

    fix: If Application not focus Keyviewer not work

--- JipperResourcePack/Keyviewer/KeyViewer.cs
+++ JipperResourcePack/Keyviewer/KeyViewer.cs
@@ -700,45 +700,43 @@ public class KeyViewer : Feature {
             int repeat = 0;
             while(KeyinputListener is { IsAlive: true } && Enabled) {
                 long elapsedMilliseconds = Stopwatch.ElapsedMilliseconds;
-                if(Application.isFocused) {
-                    KeyCode[] keyCodes = GetKeyCode();
-                    for(int i = 0; i < keyCodes.Length; i++) {
-                        bool current = CheckKey(keyCodes[i]);
-                        Key key = Keys[i];
-                        if(!key) continue;
-                        for(int j = 0; j < key.rainList.Count; j++) {
-                            RawRain rain = key.rainList[j];
-                            if(rain.UpdateLocation(elapsedMilliseconds, current && keyState[i] && j == key.rainList.Count - 1)) continue;
-                            key.rainList.Remove(rain);
-                            rain.removed = true;
-                            j--;
-                        }
-                        if(current == keyState[i]) continue;
-                        keyState[i] = current;
-                        UpdateKey(i, current);
-                        if(!current) continue;
-                        key.value.text = (++settings.Count[i]).ToString();
-                        Total.value.text = (++settings.TotalCount).ToString();
-                        PressTimes.Enqueue(elapsedMilliseconds);
-                        RawRain rawRain = new(key.rain.transform, elapsedMilliseconds, key.color);
-                        key.rawRainQueue.Enqueue(rawRain);
-                        key.rainList.Add(rawRain);
-                        Save = true;
+                KeyCode[] keyCodes = GetKeyCode();
+                for(int i = 0; i < keyCodes.Length; i++) {
+                    bool current = CheckKey(keyCodes[i]);
+                    Key key = Keys[i];
+                    if(!key) continue;
+                    for(int j = 0; j < key.rainList.Count; j++) {
+                        RawRain rain = key.rainList[j];
+                        if(rain.UpdateLocation(elapsedMilliseconds, current && keyState[i] && j == key.rainList.Count - 1)) continue;
+                        key.rainList.Remove(rain);
+                        rain.removed = true;
+                        j--;
                     }
-                    keyCodes = GetFootKeyCode();
-                    for(int i = 0; i < keyCodes.Length; i++) {
-                        bool current = CheckKey(keyCodes[i]);
-                        int index = i + 20;
-                        Key key = Keys[index];
-                        if(!key || current == keyState[index]) continue;
-                        keyState[index] = current;
-                        UpdateKey(index, current);
-                        if(!current) continue;
-                        PressTimes.Enqueue(elapsedMilliseconds);
-                        settings.Count[index]++;
-                        Total.value.text = (++settings.TotalCount).ToString();
-                        Save = true;
-                    }
+                    if(current == keyState[i]) continue;
+                    keyState[i] = current;
+                    UpdateKey(i, current);
+                    if(!current) continue;
+                    key.value.text = (++settings.Count[i]).ToString();
+                    Total.value.text = (++settings.TotalCount).ToString();
+                    PressTimes.Enqueue(elapsedMilliseconds);
+                    RawRain rawRain = new(key.rain.transform, elapsedMilliseconds, key.color);
+                    key.rawRainQueue.Enqueue(rawRain);
+                    key.rainList.Add(rawRain);
+                    Save = true;
+                }
+                keyCodes = GetFootKeyCode();
+                for(int i = 0; i < keyCodes.Length; i++) {
+                    bool current = CheckKey(keyCodes[i]);
+                    int index = i + 20;
+                    Key key = Keys[index];
+                    if(!key || current == keyState[index]) continue;
+                    keyState[index] = current;
+                    UpdateKey(index, current);
+                    if(!current) continue;
+                    PressTimes.Enqueue(elapsedMilliseconds);
+                    settings.Count[index]++;
+                    Total.value.text = (++settings.TotalCount).ToString();
+                    Save = true;
                 }
                 while(PressTimes.TryPeek(out long result)) {
                     if(elapsedMilliseconds - result > 1000)
