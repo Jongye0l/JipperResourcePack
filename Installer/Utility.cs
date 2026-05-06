87fc687b2e53cd0c57253324e919168eba66c9ed
Jongyeol <bcpjy1233@gmail.com>
2026-05-06 15:01:15 +0900

    fix: invalid adofai folder find

--- Installer/Utility.cs
+++ Installer/Utility.cs
@@ -4,19 +4,23 @@ using Microsoft.Win32;
 
 namespace JipperResourcePack.Installer;
 
-public class Utility {
+public static class Utility {
     public static string GetAdofaiPath() {
         string steamPath;
         using(RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\" + (Environment.Is64BitOperatingSystem ? "Wow6432Node\\" : "") + "Valve\\Steam"))
             steamPath = key?.GetValue("InstallPath") as string;
         if(steamPath != null) {
             string path = Path.Combine(steamPath, "steamapps", "common", "A Dance of Fire and Ice");
-            if(Directory.Exists(path)) return path;
+            if(CheckAdofaiDirectory(path)) return path;
         }
         for(char c = 'A'; c <= 'Z'; c++) {
             string path = c + ":/SteamLibrary/steamapps/common/A Dance of Fire and Ice";
-            if(Directory.Exists(path)) return path;
+            if(CheckAdofaiDirectory(path)) return path;
         }
         return null;
+    }
+
+    private static bool CheckAdofaiDirectory(string path) {
+        return Directory.Exists(Path.Combine(path, "A Dance of Fire and Ice_Data")) && File.Exists(Path.Combine(path, "A Dance of Fire and Ice.exe"));
     }
 }
